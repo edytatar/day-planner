6d13162fc2d41ca43e85250b71f0532440c56189
edytatar <edytatar@gmail.com>
2021-10-31 16:27:40 -0500

    Added additional comments and made code more readable

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 // Declare variables
-
 var currentDay = $("#currentDay");
 var timeBlocks = $(".time-block");
 var scheduleArea = $(".schedule");
 
 var toDoItems = [];
- var today = moment();
+var today = moment();
+
 var currentDate = (today.format("LL"));
 var currentHour = today.format("H");
 
 // If no To-Dos, create array of objects
-function startSchedule(){
+function startSchedule() {
 
-//for every time block 
-  timeBlocks.each(function(){
+  //for every time block 
+  timeBlocks.each(function () {
     var $thisBlock = $(this);
     var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
 
+    // Creatie To Do object
     var todoObj = {
       // Sets To-Do hour to same as data-hour
       hour: thisBlockHr,
@@ -32,46 +33,44 @@ function startSchedule(){
 }
 
 // Change color based on hour
-function setUpTimeBlocks(){
-    timeBlocks.each(function(){
-      var $thisBlock = $(this);
-      var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
+function changeColorHour() {
+  timeBlocks.each(function () {
+    var $thisBlock = $(this);
+    var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
 
-      //add style to time blocks to show where we are in the day
-      if (thisBlockHr == currentHour) {
-        $thisBlock.addClass("present")
-      }
-      if (thisBlockHr < currentHour) {
-        $thisBlock.addClass("past")
-      }
-      if (thisBlockHr > currentHour) {
-        $thisBlock.addClass("future")
-      }
-    });
+    //add style to time blocks to show where we are in the day
+    if (thisBlockHr == currentHour) {
+      $thisBlock.addClass("present");
+    }
+    if (thisBlockHr < currentHour) {
+      $thisBlock.addClass("past");
+    }
+    if (thisBlockHr > currentHour) {
+      $thisBlock.addClass("future")
+    }
+  });
 }
 
-function renderSchedule(){
+function renderSchedule() {
   toDoItems = localStorage.getItem("todos");
   toDoItems = JSON.parse(toDoItems);
 
-  for (var i = 0; i < toDoItems.length; i++){
+  for (var i = 0; i < toDoItems.length; i++) {
     var itemHour = toDoItems[i].hour;
-    var itemText = toDoItems[i].text; 
-   
+    var itemText = toDoItems[i].text;
+
     $("[data-hour=" + itemHour + "]").children("textarea").val(itemText);
   }
-
-  console.log(toDoItems);
 }
 
-function saveHandler(){
+function saveHandler() {
   var $thisBlock = $(this).parent();
 
   var hourToUpdate = $(this).parent().attr("data-hour");
   var itemToAdd = (($(this).parent()).children("textarea")).val();
 
-  for (var j = 0; j < toDoItems.length; j++){
-    if (toDoItems[j].hour == hourToUpdate){
+  for (var j = 0; j < toDoItems.length; j++) {
+    if (toDoItems[j].hour == hourToUpdate) {
       //set its text to what was added to textarea
       toDoItems[j].text = itemToAdd;
     }
@@ -81,19 +80,19 @@ function saveHandler(){
 }
 
 // On doc load
-$(document).ready(function(){
+$(document).ready(function () {
   // Format time-blocks
-  setUpTimeBlocks();
-  
-  if(!localStorage.getItem("todos")){
+  changeColorHour();
+
+  if (!localStorage.getItem("todos")) {
     startSchedule();
-  } 
+  }
   currentDay.text(currentDate);
 
   renderSchedule();
 
   scheduleArea.on("click", "button", saveHandler);
-  
+
 });
 
 
